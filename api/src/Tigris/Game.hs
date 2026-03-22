f9ee0d57cd8471f1afd5952807aef9d06566b170
JoeCordingley <joewcordingley@gmail.com>
2026-03-22 07:19:43 +0000

    wip

--- api/src/Tigris/Game.hs
+++ api/src/Tigris/Game.hs
@@ -14,8 +14,9 @@ import Data.Aeson (FromJSON, ToJSON)
 import Data.Bool (bool)
 import Data.List (sort)
 import Control.Monad.State (StateT(..))
+import Data.Monoid (Sum(..))
 
-data Sphere = Settlements | Temples | Farms | Markets deriving (Show, Eq, Ord)
+type Sphere = Tile
 
 data Dynasty = Archer | Bull | Pot | Lion deriving (Eq, Ord, Generic, Show)
 instance FromJSON Dynasty
@@ -30,21 +31,38 @@ data Game = Game {bag :: [Tile], players :: Map Dynasty PlayerInfo, board :: Boa
 
 data Board = Board {numberOfTreasuresLeft :: Int} deriving Show
 
-data PlayerInfo = PlayerInfor {score :: Score, hand :: [Tile]} deriving Show
+data PlayerInfo = PlayerInfor {score :: Score, hand :: Bag Tile} deriving Show
 
-data Tile = Tile deriving Show
+type Bag a = Map a (Sum Int)
+
+type Hand = Bag Tile
+
+one :: a -> Bag a 
+one a = Map.singleton a (Sum 1)
+
+data Tile = Temple | Market | Settlement | Farm deriving (Show, Eq, Ord)
 
 startingBoard :: Board
 startingBoard = undefined
+
+allCivilizationTiles :: Bag Tile
+allCivilizationTiles = Map.fromList[(Temple, 57), (Market, 30), (Settlement, 30), (Farm,  36)]
+
+tilesMinusStartingTemples :: Bag Tile
+tilesMinusStartingTemples = allCivilizationTiles <> Map.singleton Temple (-10)
+
+bagToList :: Bag a -> [a]
+bagToList = Map.foldrWithKey f [] where
+  f k (Sum n) ts = replicate n k <> ts
 
 type Score = Map Sphere Int
 
 type Scores = Map Dynasty Score
 
 setupGame :: (MonadRandom m) => Map Dynasty a -> m PlayingState
-setupGame m = fromShuffled <$> shuffleM dynasties
+setupGame m = fromShuffled <$> shuffleM dynasties <*> shuffleM (bagToList tilesMinusStartingTemples)
   where
-    fromShuffled dynasties' = PlayingState {turnOrder = cycle dynasties, game = startingGame} where
+    fromShuffled dynasties' tiles = PlayingState {turnOrder = cycle dynasties, game = startingGame} where
       startingGame = Game {players = Map.fromList $ map (,startingPlayerInfo) dynasties', board = startingBoard}
     dynasties = Map.keys m
 
@@ -55,6 +73,7 @@ winners finalScores' = snd . Map.findMax $ Map.foldMapWithKey groupByScore final
   groupByScore k v = Map.singleton (sort $ Map.elems v) [k]
 
 data Pass
+
 startingPlayerInfo :: PlayerInfo
 startingPlayerInfo = undefined
 
@@ -62,7 +81,7 @@ startingScore :: Score
 startingScore = Map.fromList $ map (,0) spheres
 
 spheres :: [Sphere]
-spheres = [Settlements, Temples, Farms, Markets]
+spheres = [Settlement, Temple, Farm, Market]
 
 playGame :: Monad m => (Winners -> m a) -> (PlayingState -> m a) -> PlayingState -> m a
 playGame finish recurse (PlayingState (player : subsequentPlayers) game) = playTurn getTurn player game >>= either finish (recurse . PlayingState subsequentPlayers)
@@ -93,19 +112,19 @@ isFinished board = numberOfTreasuresLeft board <= 2
 
 endTurn :: Game -> Either Winners Game
 endTurn = orDetermineWinners . playerAndBag . uncurry $ deals dealUpToSix where
-  dealUpToSix playerTiles bag = foldr (>=>) pure (replicate (6 - length playerTiles) dealOne) (playerTiles, bag) where
-    dealOne (playerTiles, x:xs) = Just (x:playerTiles, xs)
-    dealOne (_,[]) = Nothing
-  deals f = runStateT . (traverse . playerHand $ StateT . f)
 
-gamePlayers :: Functor f => (PlayerInfos -> f PlayerInfos) -> Game -> f Game
-gamePlayers f game = g <$> f (players game) where
-  g playerInfos = game {players = playerInfos}
+dealUpToSix :: Hand -> [Tile] -> Maybe (Hand, [Tile])
+dealUpToSix playerTiles bag = foldr (>=>) pure (replicate (6 - length playerTiles) dealOne) (playerTiles, bag) where
+  dealOne (playerTiles, x:xs) = Just (one x <> playerTiles, xs)
+  dealOne (_,[]) = Nothing
+
+deals :: (Monad m) => (Hand -> [Tile] -> m (Hand, [Tile])) -> PlayerInfos -> [Tile] -> m (PlayerInfos, [Tile])
+deals f = runStateT . (traverse . playerHand $ StateT . f)
 
 playerAndBag :: Functor f => ((PlayerInfos, [Tile]) -> f (PlayerInfos, [Tile])) -> Game -> f Game
 playerAndBag f game = uncurry g <$> f (players game, bag game) where
   g players bag = game{players, bag}
 
-playerHand :: Functor f => ([Tile] -> f [Tile]) -> PlayerInfo -> f PlayerInfo
+playerHand :: Functor f => (Hand -> f Hand) -> PlayerInfo -> f PlayerInfo
 playerHand f playerInfo = fmap g . f $ hand playerInfo where
   g hand = playerInfo{hand}
